9145040885da92edcbc006f37cf5e301e37255d9
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2019-12-03 16:47:55 +0100

    Changed for implemantation
    Obj -> obj; comment command complete function

--- @wsg50/ack.m
+++ @wsg50/ack.m
@@ -2,18 +2,18 @@
 
 %   Copyright 2019 Fachhochschule Dortmund LIT
 
-function ack(Obj)
+function ack(obj)
 
     %Flags Input check
     ErrorCode = 0;
 
     if ErrorCode == 0
-            Obj.ID = '24';                              %ID Graps
-            Obj.Payload = ['03'; '00'];                 %Payload length grasp
-            Obj.Command = ['61'; '63'; '6B'];           %No Command
+            obj.ID = '24';                              %ID Graps
+            obj.Payload = ['03'; '00'];                 %Payload length grasp
+            obj.Command = ['61'; '63'; '6B'];           %No Command
 
-            DataEncode(Obj);
-            DataSend(Obj);
-            command_complete(Obj);
+            DataEncode(obj);
+            DataSend(obj);
+            %command_complete(obj);
     end
 end
