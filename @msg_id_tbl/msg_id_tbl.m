03968a9042225a22bd53fcc1f8a6e78996a7943c
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2019-12-03 16:53:17 +0100

    Edit Function, Added Function
    Edited Function: New ID, Added Function: Enter ID Values

--- @msg_id_tbl/msg_id_tbl.m
+++ @msg_id_tbl/msg_id_tbl.m
@@ -1,25 +1,29 @@
 classdef msg_id_tbl < handle
-  %MAG_ID_TBL Summary of this class goes here
-  %   Detailed explanation goes here
-  
-  properties
-    active
-    msg_tbl = struct;
-  end
-  
-  methods
-    function obj = mag_id_tbl(varargin)
-      %MAG_ID_TBL Construct an instance of this class
-      %   Detailed explanation goes here
-      obj.active = true;
-    end
-    
-    function new_ID(obj,ID_num,command)
-      %METHOD1 Summary of this method goes here
-      %   Detailed explanation goes here
-      ID_num = strcat('ID_',ID_num);
-      obj.msg_tbl.(ID_num) = command; 
-    end
-  end
+	%MAG_ID_TBL Summary of this class goes here
+	%   Detailed explanation goes here
+	
+	properties
+		active
+		msg_tbl = struct;
+	end
+	
+	methods
+		function obj = mag_id_tbl(varargin)
+			%MAG_ID_TBL Construct an instance of this class
+			%   Detailed explanation goes here
+			obj.active = true;
+		end
+		
+		function new_ID(obj,ID_num)
+			%METHOD1 Summary of this method goes here
+			%   Detailed explanation goes here
+			ID_entry = strcat('ID_',dec2hex(ID_num,2));
+			obj.msg_tbl.(ID_entry).('ID') = ID_num;
+		end
+		
+		function enter_ID_val(obj,ID_num,field,value)
+			obj.msg_tbl.(dec2hex(ID_num,2)).(field) = value;
+		end
+	end
 end
 
