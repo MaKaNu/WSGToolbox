bc3865187093c006a3cccc4021cb69671de5be31
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2019-12-13 14:23:40 +0100

    Extended the FillRespondTable
    Corrected and added the Cmds from '06' to '73'

--- @msg_id_tbl/msg_id_tbl.m
+++ @msg_id_tbl/msg_id_tbl.m
@@ -46,12 +46,253 @@ classdef msg_id_tbl < handle
 		
 		function FillRespondTable(obj)
 			%Fill Respond of Command ID 06h
-			obj.respond_value_tbl.Type{1} = {'INTEGER'};
-			obj.respond_value_tbl.TypeLength{1} = 5; %NEED TO BE FIXED
-			obj.respond_value_tbl.Num_CMD(1) = 1;
-			obj.respond_value_tbl.Symbol{1} = {'LOOPDATA'};
-			obj.respond_value_tbl.Name{1} = {'Your Entry: '};
-			obj.respond_value_tbl.Unit{1} = {''};
+			obj.respond_value_tbl{'ID_06','Type'}{:} = {
+				'INTEGER'};
+			obj.respond_value_tbl{'ID_06','TypeLength'}{:} = {5}; %NEED TO BE FIXED
+			NUM_CMD = size(obj.respond_value_tbl{'ID_06','Type'}{:},2);
+			obj.respond_value_tbl{'ID_06','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_06','Symbol'}{:} = {
+				'LOOPDATA'};
+			obj.respond_value_tbl{'ID_06','Name'}{:} = {
+				'Your Entry: '};
+			obj.respond_value_tbl{'ID_06','Unit'}{:} = {''};
+			
+			%Fill Respond of Command ID 31h
+			obj.respond_value_tbl{'ID_31','Type'}{:} = {
+				'FLOAT'};
+			obj.respond_value_tbl{'ID_31','TypeLength'}{:} = {4}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_31','Type'}{:},2);
+			obj.respond_value_tbl{'ID_31','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_31','Symbol'}{:} = {
+				'ACC'};
+			obj.respond_value_tbl{'ID_31','Name'}{:} = {
+				'Acceleration: '};
+			obj.respond_value_tbl{'ID_31','Unit'}{:} = {' mm/s^2'};
+			
+			%Fill Respond of Command ID 33h
+			obj.respond_value_tbl{'ID_33','Type'}{:} = {
+				'FLOAT'};
+			obj.respond_value_tbl{'ID_33','TypeLength'}{:} = {4}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_33','Type'}{:},2);
+			obj.respond_value_tbl{'ID_33','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_33','Symbol'}{:} = {
+				'FORCE'};
+			obj.respond_value_tbl{'ID_33','Name'}{:} = {'Force: '};
+			obj.respond_value_tbl{'ID_33','Unit'}{:} = {' N'};
+			
+			%Fill Respond of Command ID 35h
+			obj.respond_value_tbl{'ID_35','Type'}{:} = {
+				'FLOAT', 'FLOAT'};
+			obj.respond_value_tbl{'ID_35','TypeLength'}{:} = {4, 4}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_35','Type'}{:},2);
+			obj.respond_value_tbl{'ID_35','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_35','Symbol'}{:} = {
+				'LIMIT_MINUS','LIMIT_PLUS'};
+			obj.respond_value_tbl{'ID_35','Name'}{:} = {
+				'Inner Limit: ', 'Outer Limit: '};
+			obj.respond_value_tbl{'ID_35','Unit'}{:} = {' mm', ' mm'};
+			
+			%Fill Respond of Command ID 40h
+			obj.respond_value_tbl{'ID_40','Type'}{:} = {
+				'BITVEC'};
+			obj.respond_value_tbl{'ID_40','TypeLength'}{:} = {4}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_40','Type'}{:},2);
+			obj.respond_value_tbl{'ID_40','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_40','Symbol'}{:} = {
+				'SSTATE'};
+			obj.respond_value_tbl{'ID_40','Name'}{:} = {
+				'System status: '};
+			obj.respond_value_tbl{'ID_40','Unit'}{:} = {''};
+			
+			%Fill Respond of Command ID 41h
+			obj.respond_value_tbl{'ID_41','Type'}{:} = {
+				'ENUM'};
+			obj.respond_value_tbl{'ID_41','TypeLength'}{:} = {1}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_41','Type'}{:},2);
+			obj.respond_value_tbl{'ID_41','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_41','Symbol'}{:} = {
+				'GSTATE'};
+			obj.respond_value_tbl{'ID_41','Name'}{:} = {
+				'Gripper status: '};
+			obj.respond_value_tbl{'ID_41','Unit'}{:} = {''};
+		
+			%Fill Respond of Command ID 42h
+			obj.respond_value_tbl{'ID_42','Type'}{:} = {
+				'INTEGER', 'INTEGER', 'INTEGER'};
+			obj.respond_value_tbl{'ID_42','TypeLength'}{:} = {4, 2, 2}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_42','Type'}{:},2);
+			obj.respond_value_tbl{'ID_42','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_42','Symbol'}{:} = {
+				'TOTAL', 'NO_PART', 'LOST_PART'};
+			obj.respond_value_tbl{'ID_42','Name'}{:} = {
+				'Number grasps: ','Number no part Found: ',...
+				'Number lost parts: '};
+			obj.respond_value_tbl{'ID_42','Unit'}{:} = {'', '', ''};
+
+			%Fill Respond of Command ID 43h
+			obj.respond_value_tbl{'ID_43','Type'}{:} = {
+				'FLOAT'};
+			obj.respond_value_tbl{'ID_43','TypeLength'}{:} = {4}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_41','Type'}{:},2);
+			obj.respond_value_tbl{'ID_43','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_43','Symbol'}{:} = {
+				'WIDTH'};
+			obj.respond_value_tbl{'ID_43','Name'}{:} = {
+				'Opening width: '};
+			obj.respond_value_tbl{'ID_43','Unit'}{:} = {' mm'};
+			
+			%Fill Respond of Command ID 44h
+			obj.respond_value_tbl{'ID_44','Type'}{:} = {
+				'FLOAT'};
+			obj.respond_value_tbl{'ID_44','TypeLength'}{:} = {4}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_44','Type'}{:},2);
+			obj.respond_value_tbl{'ID_44','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_44','Symbol'}{:} = {
+				'SPEED'};
+			obj.respond_value_tbl{'ID_44','Name'}{:} = {
+				'Finger velocity: '};
+			obj.respond_value_tbl{'ID_44','Unit'}{:} = {' mm/s'};
+
+			%Fill Respond of Command ID 45h
+			obj.respond_value_tbl{'ID_45','Type'}{:} = {
+				'FLOAT'};
+			obj.respond_value_tbl{'ID_45','TypeLength'}{:} = {4}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_45','Type'}{:},2);
+			obj.respond_value_tbl{'ID_45','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_45','Symbol'}{:} = {
+				'FINGERFORCE'};
+			obj.respond_value_tbl{'ID_45','Name'}{:} = {
+				'Finger force: '};
+			obj.respond_value_tbl{'ID_45','Unit'}{:} = {' N'};
+			
+			%Fill Respond of Command ID 46h
+			obj.respond_value_tbl{'ID_46','Type'}{:} = {
+				'FLOAT'};
+			obj.respond_value_tbl{'ID_46','TypeLength'}{:} = {2}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_46','Type'}{:},2);
+			obj.respond_value_tbl{'ID_46','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_46','Symbol'}{:} = {
+				'TEMP'};
+			obj.respond_value_tbl{'ID_46','Name'}{:} = {
+				'Temperature: '};
+			obj.respond_value_tbl{'ID_46','Unit'}{:} = {' �C'};
+			
+			%Fill Respond of Command ID 50h
+			obj.respond_value_tbl{'ID_50','Type'}{:} = {
+				'ENUM', 'INTEGER','INTEGER', 'INTEGER'};
+			obj.respond_value_tbl{'ID_50','TypeLength'}{:} = {1, 1, 2, 4}; 
+			NUM_CMD = size(obj.respond_value_tbl{'ID_46','Type'}{:},2);
+			obj.respond_value_tbl{'ID_50','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_50','Symbol'}{:} = {
+				'TYPE', 'HWREV','FW_VERSION', 'SN'};
+			obj.respond_value_tbl{'ID_50','Name'}{:} = {
+				'Device type: ','Hardware revision: ',...
+				'Firmware version: ', 'Serialnumber: '};
+			obj.respond_value_tbl{'ID_50','Unit'}{:} = {'','','',''};
+			
+			%Fill Respond of Command ID 52h
+			obj.respond_value_tbl{'ID_52','Type'}{:} = {
+				'STRING'};
+			obj.respond_value_tbl{'ID_52','TypeLength'}{:} = {4}; %NEED TO BE FIXED
+			NUM_CMD = size(obj.respond_value_tbl{'ID_52','Type'}{:},2);
+			obj.respond_value_tbl{'ID_52','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_52','Symbol'}{:} = {
+				'TAG'};
+			obj.respond_value_tbl{'ID_52','Name'}{:} = {
+				'Device tag: '};
+			obj.respond_value_tbl{'ID_52','Unit'}{:} = {''};
+			
+			%Fill Respond of Command ID 53h
+			obj.respond_value_tbl{'ID_53','Type'}{:} = {
+				'FLOAT','FLOAT','FLOAT','FLOAT',...
+				'FLOAT','FLOAT','FLOAT','FLOAT'};
+			obj.respond_value_tbl{'ID_53','TypeLength'}{:} = {
+				4,4,4,4,4,4,4,4};
+			NUM_CMD = size(obj.respond_value_tbl{'ID_53','Type'}{:},2);
+			obj.respond_value_tbl{'ID_53','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_53','Symbol'}{:} = {
+				'TOTALSTROKE', 'MIN_SPEED', 'MAX_SPEED','MIN_ACC',...
+				'MAX_ACC', 'MIN_FORCE','NOM_FORCE', 'OVR_FORCE' };
+			obj.respond_value_tbl{'ID_53','Name'}{:} = {
+				'Total width: ', 'Minimum velocity: ', 'Maximum velocity: ',...
+				'Minimum acceleration: ','Maximum acceleration: ',...
+				'Minimum force: ', 'Nominal force: ', 'Overdrive Force: '};
+			obj.respond_value_tbl{'ID_53','Unit'}{:} = {
+				' mm', ' mm/s', ' mm/s', ' mm/s²',....
+				' mm/s^2', ' N', ' N', ' N'};
+		
+			%Fill Respond of Command ID 60h
+			obj.respond_value_tbl{'ID_60','Type'}{:} = {
+				'ENUM', 'INTEGER'};
+			obj.respond_value_tbl{'ID_60','TypeLength'}{:} = {1,2};
+			NUM_CMD = size(obj.respond_value_tbl{'ID_60','Type'}{:},2);
+			obj.respond_value_tbl{'ID_60','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_60','Symbol'}{:} = {
+				'FINGERTYPE1', 'SIZE1'};
+			obj.respond_value_tbl{'ID_60','Name'}{:} = {
+				'Finger 1 type: ', 'Data Size: '};
+			obj.respond_value_tbl{'ID_60','Unit'}{:} = {'', ''};
+			
+			%Fill Respond of Command ID 61h
+			obj.respond_value_tbl{'ID_61','Type'}{:} = {
+				'BITVEKTOR'};
+			obj.respond_value_tbl{'ID_61','TypeLength'}{:} = {2};
+			NUM_CMD = size(obj.respond_value_tbl{'ID_61','Type'}{:},2);
+			obj.respond_value_tbl{'ID_61','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_61','Symbol'}{:} = {
+				'FLAGS1'};
+			obj.respond_value_tbl{'ID_61','Name'}{:} = {
+				'Finger 1 Flags: '};
+			obj.respond_value_tbl{'ID_61','Unit'}{:} = {''};
+			
+			%Fill Respond of Command ID 63h
+			obj.respond_value_tbl{'ID_63','Type'}{:} = {
+				'STRING'};
+			obj.respond_value_tbl{'ID_63','TypeLength'}{:} = {4}; %NEED TO BE FIXED
+			NUM_CMD = size(obj.respond_value_tbl{'ID_63','Type'}{:},2);
+			obj.respond_value_tbl{'ID_63','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_63','Symbol'}{:} = {
+				'FINGER1'};
+			obj.respond_value_tbl{'ID_63','Name'}{:} = {
+				'specific '};
+			obj.respond_value_tbl{'ID_63','Unit'}{:} = {''};
+		
+			%Fill Respond of Command ID 70h
+			obj.respond_value_tbl{'ID_70','Type'}{:} = {
+				'ENUM', 'INTEGER'};
+			obj.respond_value_tbl{'ID_70','TypeLength'}{:} = {1,2};
+			NUM_CMD = size(obj.respond_value_tbl{'ID_70','Type'}{:},2);
+			obj.respond_value_tbl{'ID_70','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_70','Symbol'}{:} = {
+				'FINGERTYPE2', 'SIZE2'};
+			obj.respond_value_tbl{'ID_70','Name'}{:} = {
+				'Finger 2 type: ', 'Data Size: '};
+			obj.respond_value_tbl{'ID_70','Unit'}{:} = {'', ''};
+			
+			%Fill Respond of Command ID 71h
+			obj.respond_value_tbl{'ID_71','Type'}{:} = {
+				'BITVEKTOR'};
+			obj.respond_value_tbl{'ID_71','TypeLength'}{:} = {2};
+			NUM_CMD = size(obj.respond_value_tbl{'ID_71','Type'}{:},2);
+			obj.respond_value_tbl{'ID_71','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_71','Symbol'}{:} = {
+				'FLAGS2'};
+			obj.respond_value_tbl{'ID_71','Name'}{:} = {
+				'Finger 2 Flags: '};
+			obj.respond_value_tbl{'ID_71','Unit'}{:} = {''};
+			
+			%Fill Respond of Command ID 73h
+			obj.respond_value_tbl{'ID_73','Type'}{:} = {
+				'STRING'};
+			obj.respond_value_tbl{'ID_73','TypeLength'}{:} = {4}; %NEED TO BE FIXED
+			NUM_CMD = size(obj.respond_value_tbl{'ID_73','Type'}{:},2);
+			obj.respond_value_tbl{'ID_73','Num_CMD'} = NUM_CMD;
+			obj.respond_value_tbl{'ID_73','Symbol'}{:} = {
+				'FINGER2'};
+			obj.respond_value_tbl{'ID_73','Name'}{:} = {
+				'specific '};
+			obj.respond_value_tbl{'ID_73','Unit'}{:} = {''};
+
 		end
 		
 		function new_ID(obj,ID_num)
@@ -65,160 +306,6 @@ classdef msg_id_tbl < handle
 			ID_entry = strcat('ID_',dec2hex(ID_num,2));
 			obj.msg_tbl.(ID_entry).(field) = value;
 		end
-		
-		% ID_31
-		%TYPE = {'FLOAT'}
-		%TYPELENGTH = {4}
-		%NUM_CMD = 1
-		%SYMBOL = {'ACC'}
-		%NAME = {'Acceleration: '}
-		%UNIT = {' mm/s²'}
-		
-		% ID_33
-		%TYPE = {'FLOAT'}
-		%TYPELENGTH = {4}
-		%NUM_CMD = 1
-		%SYMBOL = {'FORCE'}
-		%NAME = {'Force: '}
-		%UNIT = {' N'}
-		
-		% ID_35
-		%TYPE = {'FLOAT', 'FLOAT'}
-		%TYPELENGTH = {4, 4}
-		%NUM_CMD = 2
-		%SYMBOL = {'LIMIT_MINUS', 'LIMIT_PLUS'}
-		%NAME = {'Inner Limit: ', 'Outer Limit: '}
-		%UNIT = {' mm', ' mm'}
-		
-		% ID_40
-		%TYPE = {'BITVEKTOR'}
-		%TYPELENGTH = {4}
-		%NUM_CMD = 1
-		%SYMBOL = {'SSTATE'}
-		%NAME = {'System Status: '}
-		%UNIT = {''}
-		
-		% ID_41
-		%TYPE = {'ENUM'}
-		%TYPELENGTH = {1}
-		%NUM_CMD = 1
-		%SYMBOL = {'GSTATE'}
-		%NAME = {'Gripper State: '}
-		%UNIT = {''}
-		
-		% ID_42
-		%TYPE = {'INTEGER', 'INTEGER', 'INTEGER'}
-		%TYPELENGTH = {4, 2, 2}
-		%NUM_CMD = 1
-		%SYMBOL = {'TOTAL', 'NO_PART', 'LOST_PART'}
-		%NAME = {'Number grasps: ', 'Number no part Found: ', 'Number lost parts: '}
-		%UNIT = {'', '', ''}
-		
-		% ID_43
-		%TYPE = {'FLOAT'}
-		%TYPELENGTH = {1}
-		%NUM_CMD = 1
-		%SYMBOL = {'WIDTH'}
-		%NAME = {'Opening width: '}
-		%UNIT = {' mm'}
-		
-		% ID_44
-		%TYPE = {'FLOAT'}
-		%TYPELENGTH = {1}
-		%NUM_CMD = 1
-		%SYMBOL = {'SPEED'}
-		%NAME = {'Finger velocity: '}
-		%UNIT = {' mm/s'}
-		
-		% ID_45
-		%TYPE = {'FLOAT'}
-		%TYPELENGTH = {1}
-		%NUM_CMD = 1
-		%SYMBOL = {'FINGERFORCE'}
-		%NAME = {'Finger force: '}
-		%UNIT = {' N'}
-		
-		% ID_46
-		%TYPE = {'INTEGER'}
-		%TYPELENGTH = {2}
-		%NUM_CMD = 1
-		%SYMBOL = {'TEMP'}
-		%NAME = {'Temperature: '}
-		%UNIT = {' °C'}
-		
-		% ID_50
-		%TYPE = {'ENUM', 'INTEGER', 'INTEGER', 'INTEGER'}
-		%TYPELENGTH = {1, 1, 2, 4}
-		%NUM_CMD = 4
-		%SYMBOL = {'TYPE', 'HWREV', 'FW_VERSION', 'SN'}
-		%NAME = {'Device type: ', 'Hardware revision: ', 'Firmware version: ', 'Serialnumber: '}
-		%UNIT = {'', '','',''}
-		
-		% ID_52
-		%TYPE = {'STRING'}
-		%TYPELENGTH = {n}
-		%NUM_CMD = 1
-		%SYMBOL = {'TAG'}
-		%NAME = {'Device tag: '}
-		%UNIT = {''}
-		
-		% ID_53
-		%TYPE = {'FLOAT','FLOAT','FLOAT','FLOAT','FLOAT','FLOAT','FLOAT','FLOAT'}
-		%TYPELENGTH = {4,4,4,4,4,4,4,4}
-		%NUM_CMD = 8
-		%SYMBOL = {'TOTALSTROKE', 'MIN_SPEED', 'MAX_SPEED', 'MIN_ACC', 'MAX_ACC', 'MIN_FORCE', 'MAX_FORCE', 'NOM_FORCE', 'OVR_FORCE' }
-		%NAME = {'Total width: ', 'Minimum velocity: ', 'Maximum velocity: ', 'Minimum acceleration: ','Maximum acceleration: ', 'Minimum force: ', 'Nominal force: ', 'Overdrive Force: '}
-		%UNIT = {' mm', ' mm/s', ' mm/s', ' mm/s²', ' mm/s²', ' N', ' N', ' N'}
-		
-		% ID_60
-		%TYPE = {'ENUM', 'INTEGER'}
-		%TYPELENGTH = {1,2}
-		%NUM_CMD = 2
-		%SYMBOL = {'FINGERTYPE1', 'SIZE1'}
-		%NAME = {'Finger 1 type: ', 'Data Size: '}
-		%UNIT = {'', ''}
-		
-		% ID_61
-		%TYPE = {'BITVEKTOR'}
-		%TYPELENGTH = {2}
-		%NUM_CMD = 1
-		%SYMBOL = {'FLAGS1'}
-		%NAME = {'Finger 1 Flags: '}
-		%UNIT = {''}
-		
-		% ID_63
-		%NEED TO BE TESTED CHOOSE WHICH FINGERSENSOR
-		%TYPE = {'STRING'}
-		%TYPELENGTH = {n}
-		%NUM_CMD = 1
-		%SYMBOL = {'TAG'}
-		%NAME = {'Device tag: '}
-		%UNIT = {''}
-		
-		% ID_70
-		%TYPE = {'ENUM', 'INTEGER'}
-		%TYPELENGTH = {1,2}
-		%NUM_CMD = 2
-		%SYMBOL = {'FINGERTYPE2', 'SIZE2'}
-		%NAME = {'Finger 2 type: ', 'Data Size: '}
-		%UNIT = {'', ''}
-		
-		% ID_71
-		%TYPE = {'BITVEKTOR'}
-		%TYPELENGTH = {2}
-		%NUM_CMD = 1
-		%SYMBOL = {'FLAGS2'}
-		%NAME = {'Finger 2 Flags: '}
-		%UNIT = {''}
-		
-		% ID_73
-		%NEED TO BE TESTED CHOOSE WHICH FINGERSENSOR
-		%TYPE = {'STRING'}
-		%TYPELENGTH = {n}
-		%NUM_CMD = 1
-		%SYMBOL = {'TAG'}
-		%NAME = {'Device tag: '}
-		%UNIT = {''}
 	end
 end
 
