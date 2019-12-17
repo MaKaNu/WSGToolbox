2bdcc0eba47a6c5ddba2448835924d0c23f9182a
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2019-12-17 16:06:28 +0100

    Überprüfe ob im cellstring auch ein string ist

--- @wsg50/wsg50.m
+++ @wsg50/wsg50.m
@@ -418,7 +418,7 @@ classdef wsg50 < handle
 						for j = 1:length(dec_str)
 							tmp_int =  tmp_int + dec_str(j)*255^(j-1);
 						end
-						if iscellstr(Symbol)
+						if iscellstr(isstring(Symbol))
 							obj.status.(Symbol{i}) = tmp_int;
 						else
 							error('ERROR: THIS SHOULD NOT HAPPEN!! FIX THE FUNCTION ARGUMENTS')
@@ -426,7 +426,7 @@ classdef wsg50 < handle
 					case 'FLOAT'    %TypeLength not used for FLOAT ??? What did I mean
 						dec_str = obj.msg_table.msg_tbl.(ID).PAYLOAD';
 						dec_str = dec_str(start_idx:end_idx);
-						if iscellstr(Symbol)
+						if iscellstr(isstring(Symbol))
 							obj.status.(Symbol{i})= typecast(uint8(dec_str),'single');
 						else
 							error('ERROR: THIS SHOULD NOT HAPPEN!! FIX THE FUNCTION ARGUMENTS')
@@ -443,7 +443,7 @@ classdef wsg50 < handle
 							a=a+8;
 							b=b+8;
 						end
-						if iscellstr(Symbol)
+						if iscellstr(isstring(Symbol))
 							obj.status.(Symbol{i})= fliplr(tmp_bivec);
 						else
 							error('ERROR: THIS SHOULD NOT HAPPEN!! FIX THE FUNCTION ARGUMENTS')
@@ -451,7 +451,7 @@ classdef wsg50 < handle
 					case 'ENUM'
 						dec_str = obj.msg_table.msg_tbl.(ID).PAYLOAD';
 						dec_str = dec_str(start_idx:end_idx);
-						if iscellstr(Symbol)
+						if iscellstr(isstring(Symbol))
 							obj.status.(Symbol{i})= dec_str;
 						else
 							error('ERROR: THIS SHOULD NOT HAPPEN!! FIX THE FUNCTION ARGUMENTS')
