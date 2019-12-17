9cce7493d086b6a50093cbf5923f7a2b0b5dd888
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2019-12-17 19:41:48 +0100

    Modify the Method to work with regex
    There was an undetected bug that causes the method to fail if the testdata size is uneven

--- @wsg50/loop.m
+++ @wsg50/loop.m
@@ -3,10 +3,11 @@
 %   Copyright 2019 Fachhochschule Dortmund LIT
 
 
-function loop(Obj,testdata)
-ErrorCode = 0;
+function loop(obj,testdata)
 DataLength = length(testdata);
 
+%Initial Error Check
+ErrorCode = 0;
 if isfield(obj.msg_table.msg_tbl, 'ID_06')
 	if ~(obj.msg_table.msg_tbl.ID_06.STATUS == 0)
 		obj.decode_status('06')
@@ -14,14 +15,31 @@ if isfield(obj.msg_table.msg_tbl, 'ID_06')
 	end
 end
 
-if not(size(testdata,1)==1)
+if not(size(testdata,1)==1) || ~ischar(testdata)
 	error(strcat('ERROR: Testdata has to be 1xN char.',...
 		' Error-Code #06002'))
 	ErrorCode = 2;
 end
-if (ischar(testdata) && mod(DataLength,2)==0 ...
-		&& ErrorCode == 0 && DataLength<=512)
-	DataLength = dec2hex(DataLength/2);
+try
+	hex2dec(testdata)
+catch
+	error('testdata should have just 0-9, a-f, or A-F.')
+end
+
+if ErrorCode == 0 && DataLength<=512
+	%Transform the Data to single Bytes
+	switch mod(DataLength,2)
+		case 0
+			Bytes = regexp(testdata, '\w{1,2}', 'match');
+			Bytes = cell2mat(Bytes');
+		case 1
+			Bytes = regexp(testdata(1:end-1), '\w{1,2}', 'match');
+			Bytes = cell2mat(Bytes');
+			EndByte = dec2hex(hex2dec(testdata(end)),2);
+			Bytes = [Bytes;EndByte];
+	end
+	%Calculate the Data Length in Hex
+	DataLength = dec2hex(ceil(DataLength/2));
 	switch length(DataLength)
 		case 1
 			Byte_1 = strcat('0',DataLength);
@@ -43,17 +61,12 @@ else
 end
 
 if ErrorCode == 0
-	c = [];
 	
-	for i = 1:2:length(testdata)
-		c = [c; testdata(1,i:i+1)];
-	end
+	obj.ID = '06';                         %ID Loop
+	obj.Payload = [Byte_1;Byte_2];							%Payload length
+	obj.Command = Bytes;
 	
-	Obj.ID = '06';                          %ID Loop
-	Obj.Payload = [Byte_1; Byte_2];         %Payload length
-	Obj.Command = c;
-	
-	DataEncode(Obj);
-	DataSend(Obj);
+	DataEncode(obj);
+	DataSend(obj);
 end
 end
