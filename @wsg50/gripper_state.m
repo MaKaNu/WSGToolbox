3ebb583d9c72a6aeea1ca40adecb1885c3f7c142
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2019-12-17 17:28:46 +0100

    make workable for cb function
    changed Obj to obj, deleted old command_complete function and deleted Infos for the message table

--- @wsg50/gripper_state.m
+++ @wsg50/gripper_state.m
@@ -3,83 +3,74 @@
 %   Copyright 2019 Fachhochschule Dortmund LIT
 
 
-function gripper_state(Obj,change, automatic,time)
+function gripper_state(obj,change, automatic,time)
 
 
-    %TYPE CHECK
-    ErrorCode = 0;
-    
-    %create vars for Decode_payload function 
-    Type = {'ENUM'};
-    TypeLength = {1};
-    Num_CMD = size(Type,2);
-    symbol = {'GSTATE'};
-    
-    if isnumeric(time) && (9999 >= time) && (time >= 10)
-            switch length(num2str(time))
-                case 2
-                    tmp = num2str(time);
-                    time_ = [tmp; '00'];
-                case 3
-                    tmp = num2str(time);
-                    time_ = [tmp(2:3); strcat('0', tmp(1))];
-                case 4
-                    tmp = num2str(time);
-                    time_ = [tmp(3:4); tmp(1:2)];
-                otherwise
-                    error('This is strange!');
-                    ErrorCode = 1;
-                    
-            end
-    else
-        error('time interval has to be between 10 and 9999!')  
-        ErrorCode = 1;
-    end
+%Initial Error Check
+ErrorCode = 0;
+if isfield(obj.msg_table.msg_tbl, 'ID_41')
+	if ~(obj.msg_table.msg_tbl.ID_41.STATUS == 0)
+		obj.decode_status('41')
+		ErrorCode = 1;
+	end
+end
 
-    
-    
-    
-    if ErrorCode == 0
-        Obj.ID = '41';                              %ID systemstate
-        Obj.Payload = ['03'; '00'];                 %Payload length systemstate
-        
-        if automatic && change
-            Obj.Command = ['03';time_];              
-        elseif change
-            Obj.Command = ['02';time_];             % This Option is irrelvant
-            warning('Just Because something is possible, doesnt mean it is usefull!')
-        elseif automatic
-            Obj.Command = ['01';time_];  
-        else
-            Obj.Command = ['00';time_];  
-        end
-        
-        DataEncode(Obj);
-        DataSend(Obj);
-        command_complete(Obj);
-        decode_payload(Obj,Type,TypeLength,Num_CMD,symbol);
-        if Obj.verbose
-            disp(strcat('GRIPPER STATUS: ', num2str(Obj.status.GSTATE)))
-            switch Obj.status.GSTATE
-                case 0
-                    disp('IDLE')
-                case 1
-                    disp('GRASPING')
-                case 2
-                    disp('NO PART FOUND')
-                case 3
-                    disp('PART LOST')
-                case 4
-                    disp('HOLDING')
-                case 5
-                    disp('RELEASING')
-                case 6
-                    disp('POSITIONING')
-                case 7
-                    disp('ERROR')
-                otherwise
-                    disp('RESERVED')
-            end
-        end
-    end
+%Data Check and Adjustment
+switch length(num2str(time))
+	case 2
+		tmp = num2str(time);
+		time_ = [tmp; '00'];
+	case 3
+		tmp = num2str(time);
+		time_ = [tmp(2:3); strcat('0', tmp(1))];
+	case 4
+		tmp = num2str(time);
+		time_ = [tmp(3:4); tmp(1:2)];
+	otherwise
+		error('time interval has to be between 10 and 9999!');
+end
+
+
+if ErrorCode == 0
+	obj.ID = '41';                              %ID systemstate
+	obj.Payload = ['03'; '00'];                 %Payload length systemstate
+	
+	if automatic && change
+		obj.Command = ['03';time_];
+	elseif change
+		obj.Command = ['02';time_];             % This Option is irrelvant
+		warning('Just Because something is possible, doesnt mean it is usefull!')
+	elseif automatic
+		obj.Command = ['01';time_];
+	else
+		obj.Command = ['00';time_];
+	end
+	
+	DataEncode(obj);
+	DataSend(obj);
+
+% 	if obj.verbose
+% 		disp(strcat('GRIPPER STATUS: ', num2str(obj.status.GSTATE)))
+% 		switch obj.status.GSTATE
+% 			case 0
+% 				disp('IDLE')
+% 			case 1
+% 				disp('GRASPING')
+% 			case 2
+% 				disp('NO PART FOUND')
+% 			case 3
+% 				disp('PART LOST')
+% 			case 4
+% 				disp('HOLDING')
+% 			case 5
+% 				disp('RELEASING')
+% 			case 6
+% 				disp('POSITIONING')
+% 			case 7
+% 				disp('ERROR')
+% 			otherwise
+% 				disp('RESERVED')
+% 		end
+% 	end
+end
 end
