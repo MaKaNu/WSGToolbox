98c91a65c457aacabf2cef4d0538ac6c22b2de29
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2019-12-17 16:07:18 +0100

    Initial Error check angepasst und variablen typo
    Obj => obj

--- @wsg50/release.m
+++ @wsg50/release.m
@@ -3,66 +3,72 @@
 %   Copyright 2019 Fachhochschule Dortmund LIT
 
 
-function release(Obj,width,speed)
+function release(obj,width,speed)
 
-    %TYPE CHECK
-    ErrorCode = 0;
-
-    if isa(width,'double') || isa(width,'single')
-        if width<=110.0 && width >= 0.0
-            width_hex = num2hex(single(width));
-            wid1 = width_hex(7:8); wid2 = width_hex(5:6); wid3 = width_hex(3:4); wid4 = width_hex(1:2);
-        else
-%                     msgbox('Error, your input-value is not inside the accepted range. Change the value between 0 and 110.','Error')
-            error(strcat('ERROR: Input-value is not inside the accepted range.',...
-                 ' Error-Code #26001'))
-            ErrorCode = 1;
-        end
-    else
-%                     msgbox('Error, you did not use the correct class-Type. Use instead "single" or "double".','Error')
-            error(strcat('ERROR: Variable-Type is not correct.',...
-                 ' Error-Code #26002'))
-            ErrorCode = 1;
-    end
-
-
-
-    if isa(speed,'double')
-        if speed<= 420.0 && speed>=5.0
-            speed_hex = num2hex(single(speed));
-            spd1 = speed_hex(7:8); spd2 = speed_hex(5:6); spd3 = speed_hex(3:4); spd4 = speed_hex(1:2);
-        else
-%                     msgbox('Error, your input-value is not inside the accepted range. Change the value between 5 and 420.','Error')
-            error(strcat('ERROR: Input-value is not inside the accepted range.',...
-                 ' Error-Code #25001'))
-            ErrorCode = 1;
-        end
-    elseif isa(speed,'single')
-        if speed<= 420.0 && speed>=5.0
-            speed_hex = num2hex(speed);
-            spd1 = speed_hex(7:8); spd2 = speed_hex(5:6); spd3 = speed_hex(3:4); spd4 = speed_hex(1:2);
-        else
-%                     msgbox('Error, your input-value is not inside the accepted range. Change the value between 5 and 420.','Error')
-            error(strcat('ERROR: Input-value is not inside the accepted range.',...
-                 ' Error-Code #25001'))
-            ErrorCode = 1;
-        end
-    else
-%                     msgbox('Error, you did not use the correct var-type. Use instead "single" or "double".','Error')
-            error(strcat('ERROR: Variable-Type is not correct.',...
-                 ' Error-Code #25002'))
-            ErrorCode = 1; 
-    end 
-
-    if ErrorCode == 0
-        Obj.ID = '26';                              %ID Graps
-        Obj.Payload = ['08'; '00'];                 %Payload length grasp
-        Obj.Command = [wid1; wid2; wid3; wid4;...   %Width in Float Little Endian
-                       spd1; spd2; spd3; spd4];     %Speed in Float Little Endian
-
-        DataEncode(Obj);
-        DataSend(Obj);
-        command_complete(Obj);
-    end
+%Initial Error Check
+ErrorCode = 0;
+if isfield(obj.msg_table.msg_tbl, 'ID_25')
+	if ~(obj.msg_table.msg_tbl.ID_25.STATUS == 0)
+		obj.decode_status('25')
+		ErrorCode = 1;
+	end
 end
-            
+
+if isa(width,'double') || isa(width,'single')
+	if width<=110.0 && width >= 0.0
+		width_hex = num2hex(single(width));
+		wid1 = width_hex(7:8); wid2 = width_hex(5:6); wid3 = width_hex(3:4); wid4 = width_hex(1:2);
+	else
+		%                     msgbox('Error, your input-value is not inside the accepted range. Change the value between 0 and 110.','Error')
+		error(strcat('ERROR: Input-value is not inside the accepted range.',...
+			' Error-Code #26001'))
+		ErrorCode = 1;
+	end
+else
+	%                     msgbox('Error, you did not use the correct class-Type. Use instead "single" or "double".','Error')
+	error(strcat('ERROR: Variable-Type is not correct.',...
+		' Error-Code #26002'))
+	ErrorCode = 1;
+end
+
+
+
+if isa(speed,'double')
+	if speed<= 420.0 && speed>=5.0
+		speed_hex = num2hex(single(speed));
+		spd1 = speed_hex(7:8); spd2 = speed_hex(5:6); spd3 = speed_hex(3:4); spd4 = speed_hex(1:2);
+	else
+		%                     msgbox('Error, your input-value is not inside the accepted range. Change the value between 5 and 420.','Error')
+		error(strcat('ERROR: Input-value is not inside the accepted range.',...
+			' Error-Code #25001'))
+		ErrorCode = 1;
+	end
+elseif isa(speed,'single')
+	if speed<= 420.0 && speed>=5.0
+		speed_hex = num2hex(speed);
+		spd1 = speed_hex(7:8); spd2 = speed_hex(5:6); spd3 = speed_hex(3:4); spd4 = speed_hex(1:2);
+	else
+		%                     msgbox('Error, your input-value is not inside the accepted range. Change the value between 5 and 420.','Error')
+		error(strcat('ERROR: Input-value is not inside the accepted range.',...
+			' Error-Code #25001'))
+		ErrorCode = 1;
+	end
+else
+	%                     msgbox('Error, you did not use the correct var-type. Use instead "single" or "double".','Error')
+	error(strcat('ERROR: Variable-Type is not correct.',...
+		' Error-Code #25002'))
+	ErrorCode = 1;
+end
+
+if ErrorCode == 0
+	obj.ID = '26';                              %ID Graps
+	obj.Payload = ['08'; '00'];                 %Payload length grasp
+	obj.Command = [wid1; wid2; wid3; wid4;...   %Width in Float Little Endian
+		spd1; spd2; spd3; spd4];     %Speed in Float Little Endian
+	
+	DataEncode(obj);
+	DataSend(obj);
+	
+end
+end
+
