f444e522bc2a3ff8816e9ff67f9d5ca5b46f72c2
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2020-01-21 13:58:24 +0100

    Add Version check for utility function

--- utility/SaveRespondTable.m
+++ utility/SaveRespondTable.m
@@ -1,4 +1,9 @@
 function SaveRespondTable()
+	if verLessThan('matlab', '9.4')
+		error(strcat('You are running a Matlab Version less than 2018a. ',...
+			'This utility Function is not available below Matlab 2018a'))
+	end
+
 	Types = {'cell','cell','int8','cell','cell','cell'};
 	Names = {'Type','TypeLength','Num_CMD','Symbol','Name','Unit'};
 	RespondTable = CreateRespondTable(Types,Names);
