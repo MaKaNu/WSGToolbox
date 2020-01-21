14bfda10533d1c49d768973fb0f6c2d390d3787b
Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2020-01-21 15:52:38 +0100

    ADD new methods
    IDs: 43,44,45,46

---
+++ @wsg50/gripperforce_state.m
@@ -0,0 +1,47 @@
+%gripperstate function
+
+%   Copyright 2019 Fachhochschule Dortmund LIT
+
+
+function gripperforce_state(obj,change, automatic,time)
+
+ErrorCode = 0;
+
+%Data Check and Adjustment
+switch length(num2str(time))
+	case 2
+		tmp = num2str(time);
+		time_ = [tmp; '00'];
+	case 3
+		tmp = num2str(time);
+		time_ = [tmp(2:3); strcat('0', tmp(1))];
+	case 4
+		tmp = num2str(time);
+		time_ = [tmp(3:4); tmp(1:2)];
+	otherwise
+		error('time interval has to be between 10 and 9999!');
+end
+
+
+if ErrorCode == 0
+	obj.ID = '45';                              %ID systemstate
+	obj.Payload = ['03'; '00'];                 %Payload length systemstate
+	
+	if automatic && change
+		obj.Command = ['03';time_];
+	elseif change
+		obj.Command = ['02';time_];             % This Option is irrelvant
+		warning('Just Because something is possible, doesnt mean it is usefull!')
+	elseif automatic
+		obj.Command = ['01';time_];
+		error(strcat('This will sent messages every timeinterval that',...
+			 ' may block your system. Choose another option!'))
+	else
+		obj.Command = ['00';time_];
+	end
+	
+	DataEncode(obj);
+	DataSend(obj);
+
+end
+end
